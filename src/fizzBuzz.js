2df5ededecd7abf3390f6a2eba22d14c937b5476
jebax <joel.eliot.baxter@gmail.com>
2018-10-29 16:41:17 +0000

    changed layout of fizzbuzz.js

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,10 +1,6 @@
 function FizzBuzz() {
-
 }
 
-FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
-  return (number % divisor === 0);
-}
 FizzBuzz.prototype.says = function(number) {
   if (this._isDivisibleBy(number, 15)) {
     return 'FizzBuzz';
@@ -16,4 +12,8 @@ FizzBuzz.prototype.says = function(number) {
     return 'Buzz';
   }
   return number;
+}
+
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
 }
