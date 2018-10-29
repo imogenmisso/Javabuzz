f67ec9db704d295ec3ba3514869b621964ad1284
jebax <joel.eliot.baxter@gmail.com>
2018-10-29 15:49:45 +0000

    added more tests for dividing numbers

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,11 +1,21 @@
 describe("FizzBuzz", function() {
   var fizzBuzz;
 
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  })
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      fizzBuzz = new FizzBuzz();
       expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+    })
+    it('divisible by 5', function() {
+      expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+    })
+
+    it('divisble by 15', function() {
+      expect(fizzBuzz.isDivisibleByFifteen(15)).toBe(true);
     })
   })
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -3,5 +3,15 @@ function FizzBuzz() {
 }
 
 FizzBuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 == 0);
+  return this._isDivisibleBy(number, 3);
+}
+
+FizzBuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+}
+FizzBuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+}
+FizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
 }
